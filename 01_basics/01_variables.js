16030ffe3e0b67ad8c1a916592f28dff8ea5092a
chandansr-codes <145322831+chandansr-codes@users.noreply.github.com>
2023-10-05 13:52:14 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountid= 14453
+let accountEmail = "chandan@google.com"
+var accountPassword= "12345"
+accountCity="mumbai"
+
+
+accountEmail="hbsdc@google.com"
+accountPassword="756453"
+accountCity="kolkata"
+
+console.log(accountid);
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountid, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as new version
+
+//alert(3 + 3) we are using nodejs, not browser
+
+console.log(3+3) //code readability should be high
+
+console.log("Rajput")
+
+let name="Rajput"
+let age=21
+let isloggedin= false
+
+//TYPES OF DATATYPES
+//Number  => range:-2 to power of 53
+//Bigint
+//string => ""
+//boolean
+//Null => standalone value
+//undefined => value has not beeen asssigned yet
+//symbol =>unique
+
+console.log(typeof undefined)// undefined
+console.log(typeof null)//object
