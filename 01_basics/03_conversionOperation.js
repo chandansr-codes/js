e01748a39342c0c8e516c77e177741ad839479ce
chandansr-codes <145322831+chandansr-codes@users.noreply.github.com>
2023-10-06 15:27:57 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,8 +22,46 @@ console.log(convertBoolean)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+//console.log(typeof stringNumber)
+//console.log(stringNumber)
+
+
+ //  *********************************OPERATIONS*********************************************
+ 
+ let values=3
+ let negValues= -values
+ //console.log(negValues)
+ /*
+ console.log(2+2)
+ console.log(2-2)
+ console.log(2*2)
+ console.log(2**3)// ** =>2 raise to the power 3
+ console.log(2/3)
+ console.log(2%3)
+ */
+/*
+ let str1="hello "
+ let str2 = "Rajput"
+ let str3 = str1+str2
+ console.log(str3)
+ */
+/*
+ console.log("1" + 2)
+ console.log(1 + "2")
+ console.log("1" + 2 + 2)
+ console.log(2 + 2 + "1")
+ // If string comes first then the whole equation is treated as string.
+ */
+
+ let gameCounter =100
+ ++gameCounter;
+ console.log(gameCounter)
+ // ++a the increment operator increments and returns the value before incrementing.
+ //a++ the increment operator increments and returns the value after incrementing.
+ 
+
+
+
 
 
 
