5a622f10d138724f1c56d34bb90fb8435072026c
chandansr-codes <145322831+chandansr-codes@users.noreply.github.com>
2023-10-06 13:57:39 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score= "33abc"
+/*
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInnumber = Number(score) //datatype conversion
+console.log(typeof valueInnumber );
+console.log(valueInnumber); // NaN means not a number,but it is a type of number
+*/
+/*
+let isLoggedIn= 1
+let Logged= ""
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+let convertBoolean= Boolean(Logged)
+console.log(convertBoolean)
+
+// 1=>true; 0=>false
+// "" => false
+// "rajput" =>true
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
+
+
+
+
