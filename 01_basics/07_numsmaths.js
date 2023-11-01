c6cba66c0faeb097ec93f4ece799536327b116ba
chandansr-codes <145322831+chandansr-codes@users.noreply.github.com>
2023-11-01 03:39:11 +0000

    nums and Maths

---
+++ 01_basics/07_numsmaths.js
@@ -0,0 +1,32 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2))//used to get number of digits after point
+
+const otherNumber = 123.8966
+//console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+// ++++++++++++++++++++++++++++ MATHS ++++++++++++++++
+/*console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.5))
+console.log(Math.floor(4.2))
+console.log(Math.max(3,6,7,8))
+console.log(Math.min(6,8,9,6))*/
+
+console.log(Math.random())
+console.log((Math.random()*10)+1);
+ 
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
